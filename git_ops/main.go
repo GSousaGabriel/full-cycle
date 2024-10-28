6ab4f0d39151c4dcadde8b443269f148d56b4e4a
Gabriel Sousa <gabriel.gs@sidi.org.br>
2024-10-28 10:28:57 -0300

    test

--- git_ops/main.go
+++ git_ops/main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main(){
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		w.Write([]byte("<h1>Hello World!</h1>"))
+		w.Write([]byte("<h1>Hello World Gabriel 2!</h1>"))
 	})
 	http.ListenAndServe(":8000", nil)
 }
